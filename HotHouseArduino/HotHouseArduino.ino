8ae51594995f4151220e362ba3000a257786d160
Ben Ransford <ransford@cs.umass.edu>
2011-12-26 18:05:05 -0500

    blink LED instead of serial output

--- HotHouseArduino/HotHouseArduino.ino
+++ HotHouseArduino/HotHouseArduino.ino
@@ -4,10 +4,11 @@
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
 
-// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+/* Setup a oneWire instance to communicate with any OneWire devices (not just
+   Maxim/Dallas temperature ICs) */
 OneWire oneWire(ONE_WIRE_BUS);
 
-// Pass our oneWire reference to Dallas Temperature. 
+// Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
 void setup(void)
@@ -18,16 +19,19 @@ void setup(void)
 
   // Start up the library
   sensors.begin();
+
+  pinMode(13, OUTPUT); // LED on Uno pin 13
 }
 
 void loop(void)
-{ 
-  // call sensors.requestTemperatures() to issue a global temperature 
+{
+  // call sensors.requestTemperatures() to issue a global temperature
   // request to all devices on the bus
-  Serial.print("Requesting temperatures...");
+  digitalWrite(13, HIGH);
   sensors.requestTemperatures(); // Send the command to get temperatures
-  Serial.println("DONE");
-  
-  Serial.print("Temperature for the device 1 (index 0) is: ");
-  Serial.println(sensors.getTempFByIndex(0));  
+  digitalWrite(13, LOW);
+
+  Serial.print("Temperature: ");
+  Serial.println(sensors.getTempFByIndex(0));
 }
+
