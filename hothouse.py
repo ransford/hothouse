bfaf9eded0f07cf6afd20323bcef1159dbf8afef
Ben Ransford <ransford@cs.umass.edu>
2012-01-06 11:24:54 -0500

    untested: write to logfile

--- hothouse.py
+++ hothouse.py
@@ -7,6 +7,8 @@ if len(sys.argv) != 2:
     print >> sys.stderr, "Usage: %s /dev/tty.usbXXXX" % sys.argv[0]
     sys.exit(1)
 
+logfilename = 'hothouse.log' # XXX use getopts
+
 serport = sys.argv[1]
 
 ser = serial.Serial(serport, 9600)
@@ -15,5 +17,20 @@ ser = serial.Serial(serport, 9600)
 ser.readline()
 ser.readline()
 
-while 1:
-    print ser.readline().rstrip()
+readings = ()
+
+try:
+    outfile = open(logfilename, 'a')
+except IOError:
+    print >> sys.stderr, "Unable to open %s for appending." % logfilename
+
+try:
+    while 1:
+        reading = ser.readline().rstrip()
+        readings.append(reading)
+        outfile.write("%f,%s\n" % (time.time(), reading))
+except StandardError:
+    outfile.flush()
+    outfile.close()
+except:
+    print >> sys.stderr, "Error."
